0d4b7ecac0d247f712d63b0008267dbdf73a377b
Phil Piwonka <piwonka@amazon.com>
2023-02-19 21:26:38 -0800

    More notes on bosses from running through the game
    thanks invincibility cheat for making this super easy.
    
      <cheat desc="P1 Invincibility">
        <script state="run">
          <action>maincpu.pb@C510D46=02</action>
        </script>
      </cheat>

--- mmpork.list.lua
+++ mmpork.list.lua
@@ -9,6 +9,15 @@ local BOSSES = {
     [80] = {[22] = {4,160,{[1] = 3600}} },  -- boss 1 "st1boss" phase one = part_id 22
     [74] = {[0] = {4,160,{[1] = 3600}} }, -- midboss 2 "mshipa"
     [81] = {[0] = {4,160,{[1] = 3600}} }, -- boss 2 "mshipa" part 31, then 5
+    [75] = {[0] = {4,160,{[1] = 3600}} }, -- midboss 3 "mshipa"
+    [82] = {[0] = {4,160,{[1] = 3600}} }, -- boss 3 "mshipa" part_id 0 for 1st phase, then part_id 1 for second phase. phase value stays at 1.
+    [76] = {[0] = {4,160,{[1] = 3600}} }, -- midboss 4.1 "mshipa" part_id 45 then part_id 0
+    [77] = {[0] = {4,160,{[1] = 3600}} }, -- midboss 4.2 "mshipa" just part_id 0
+    [83] = {[0] = {4,160,{[1] = 3600}} }, -- boss 4 62 -> 48 (phase 3 on 0, on 48 phase = 4?) -> 0 (phase = 5)
+    [79] = {[0] = {4,160,{[1] = 3600}} }, -- midboss 5.1 seems like it's just 0
+    [78] = {[0] = {4,160,{[1] = 3600}} }, -- midboss 5.2 seems like it's just 0
+    [21] = {[0] = {4,160,{[1] = 3600}} }, -- stage 5 THE PAINTING part_id 3
+    [84] = {[0] = {4,160,{[1] = 3600}} }, -- boss 5 phase 1 = part_id 56, phase 3 part_id 26, phase 5 = part_id 0, phase 2 = part_id 42
 }
 
 function list_enemies()
@@ -21,8 +30,8 @@ function list_enemies()
     enemy_part_id2 = p:read_u32(enemy_ptr + 16);
     enemy_status = p:read_u32(enemy_ptr);
     enemy_hitpoints = p:read_u16(enemy_ptr + 68); -- this is right, I blew up midboss1 when the HP (on some enemy) hit zero
-    --enemy_phase = p:read_u8(enemy_ptr + 180);
-    enemy_phase = p:read_u8(enemy_ptr + 192);
+    enemy_phase = p:read_u8(enemy_ptr + 180);
+    --enemy_phase = p:read_u8(enemy_ptr + 192);
     if enemy_status & 0x80000000 ~= 0 and enemy_status & 0x4000 == 0 and enemy_hitpoints > 0 then
       print(enemy_idx .. " base: " .. enemy_status .. " t: " .. enemy_type .. " part_id: " .. enemy_part_id .. " part_id2: " .. enemy_part_id2 .. " hp: " .. enemy_hitpoints .. " phase: " .. enemy_phase);
     end
